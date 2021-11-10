43f02aaff1509b20ce776bfeb9cc12b8acd8db19
Sihayah <sihaya_a@hotmail.com>
2021-11-10 15:09:03 -0500

    language selection

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -18,6 +19,13 @@ var formSubmitHandler = function(event) {
 
 }; 
 
+  var buttonClickHandler =function(event){
+      var language = event.target.getAttribute("data-language")
+      if(language) {
+          getFeaturedRepos(language);
+          repoContainerEl.textContent = "";
+      }
+  }
 var displayRepos = function(repos, searchTerm) {
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
@@ -71,8 +79,23 @@ var getUserRepos = function(user) {
     })
   };
 
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language); 
+            })
+        } else {
+            alert('Error: Github User Not Found')
+        }
+    })
+  };
 
 
 
 
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 userFormEl.addEventListener("submit", formSubmitHandler);
